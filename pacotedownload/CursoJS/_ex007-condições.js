c032c68fbc366c133033d3adf11a712229e1b5f4
Mateus Cunha <mateuscunhazp@gmail.com>
2025-07-31 18:04:58 -0300

    adding course files

---
+++ pacotedownload/CursoJS/_ex007-condições.js
@@ -0,0 +1,7 @@
+//apertar F8 para rodar o node.js
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel.toFixed(2)} Km/h`)
+if (vel > 60) {
+    console.log("Você ultrapassou o limite de velocidade, logo, está MULTADO")
+}
+console.log("Dirija sempre usando o cinto de segurança :)")
---
+++ pacotedownload/CursoJS/_ex010-switch.js
@@ -0,0 +1,26 @@
+var agora = new Date()
+var dia = agora.getDay()
+switch(dia) {
+    case 0:
+        dia = "Domingo"
+    break
+    case 1:
+        dia = "Segunda"
+    break
+    case 2:
+        dia = "Terça"
+    break
+    case 3:
+        dia = "Quarta"
+    break
+    case 4:
+        dia = "Quinta"
+    break
+    case 5:
+        dia = "Sexta"
+    break
+    case 6:
+        dia = "Sábado"
+    break
+}
+console.log(dia)
