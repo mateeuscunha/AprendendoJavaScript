e5b9bbd7d1c868e30f60efa59d854413b97242fa
Mateus Cunha <mateuscunhazp@gmail.com>
2025-08-07 19:57:10 -0300

    adding images

---
+++ pacotedownload/CursoJS/horadodia - Copia/horadodia.js
@@ -0,0 +1,19 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 2
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >=0 && hora < 12) {
+        img.src = 'fotomanha.jpg'
+        document.body.style.background = '#f7d026ff'
+    } else if (hora < 18) {
+        img.src = 'fototarde.jpg'
+        document.body.style.background = '#b65f0dff'
+    }
+    else {
+        img.src = 'fotonoite.jpg'
+        document.body.style.background = '#360d77'
+    }
+}
