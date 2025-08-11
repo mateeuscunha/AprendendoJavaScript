34a43c9fbccdc5623143318764e11816d80f9dce
Mateus Cunha <mateuscunhazp@gmail.com>
2025-08-11 16:28:40 -0300

    learning about "for"

---
+++ CursoJS_Guanabara/ExAula/_ex012-for.js
@@ -0,0 +1,12 @@
+// var c = 1
+// while (c <= 10) {
+//     console.log(`Passo ${c}.`)
+//     c++
+// }
+
+//Isso equivale, em for, a isso:
+console.log(`vai começar...`)
+for(var a = 1;a <= 3;a++) {
+    console.log(`Jogada ${a}.`)
+}
+console.log(`FIM!`)
