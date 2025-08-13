86f1d02032ff5762897e61d7a4c7a75b2049c1c9
Mateus Cunha <mateuscunhazp@gmail.com>
2025-08-12 23:34:51 -0300

    fixing the model

--- CursoJS_Guanabara/ExVariados/modelo/script.js
+++
@@ -1,4 +0,0 @@
-var beginspace = window.document.getElementById("begin")
-var endspace = window.document.getElementById("end")
-var passospace = window.document.getElementById("passo")
-//Já foi testado e realmente são os espaços;
