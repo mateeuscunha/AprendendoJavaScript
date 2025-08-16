245d6e309631cfb550a159c3db61eff090f7ed5c
Mateus Cunha <mateuscunhazp@gmail.com>
2025-08-16 15:04:28 -0300

    correcting projects

--- CursoJS_Guanabara/ExVariados/contador/scriptcontador.js
+++ CursoJS_Guanabara/ExVariados/contador/scriptcontador.js
@@ -7,17 +7,30 @@ function contar() {
     //se algum dos espaços não foi preenchido
     if (beginspace.value.length == 0 || endspace.value.length == 0|| passospace.value.length == 0) {
         window.alert("Preencha corretamente o formulário.")
+        res.innerHTML = "Impossível contar!"
     } else {
-    var contagem = ""
-    var inicio = Number(beginspace.value)
-    var fim = Number(endspace.value)
-    var passo = Number(passospace.value)
-
-    for (inicio; inicio <= fim; inicio += passo) {
-        contagem += `👉 ${inicio} `
+        var contagem = ""
+        var inicio = Number(beginspace.value)
+        var fim = Number(endspace.value)
+        var passo = Number(passospace.value)
+        //consertando o passo <= 0
+        if (passo <= 0) {
+            window.alert("Passo inválido, considerando passo = 1")
+            passo = 1
+        }
+        if(inicio < fim) {
+            //contagem progressiva
+            for (inicio; inicio <= fim; inicio += passo) {
+                contagem += `👉 ${inicio} `
+            } 
+        } else {
+            //contagem regressiva
+            for (inicio; inicio >= fim; inicio -= passo)
+                contagem += `👉 ${inicio} `
+            }
+        res.innerHTML = `Contando: <br> ${contagem} 👉🏁`
     }
-    res.innerHTML = `Contando: <br> ${contagem} 👉🏁`
 }
-}
+
 
 // Esse foi o primeiro exercicio que consegui fazer sozinho em JS, o deepseek me ajudou a corrigir apenas um erro de sitaxe :)
